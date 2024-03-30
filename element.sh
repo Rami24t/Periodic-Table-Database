f99873752934971392887a4825bbaa7c963cbc72
Rami <103028944+Rami24t@users.noreply.github.com>
2024-03-31 00:03:48 +0100

    Update element.sh

--- element.sh
+++ element.sh
@@ -7,13 +7,14 @@ then
 echo Please provide an element as an argument.
 else
 
-RESULT="$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE (atomic_number::VARCHAR) = ($1::VARCHAR) OR symbol = ($1::VARCHAR) OR name = ($1::VARCHAR)")"
+RESULT="$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE (atomic_number::VARCHAR) = ('$1'::VARCHAR) OR symbol = ('$1'::VARCHAR) OR name = ('$1'::VARCHAR)")"
 
-#  #  ## echo "$RESULT"
+# echo "$RESULT"
 
-while $RESULT | read ATOMIC_NUMBER _ NAME _ SYMBOL _ TYPE _ ATOMIC_MASS _ MELTING_POINT _ BOILING_POINT
+
+echo $RESULT | while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
 do
-echo "Atomic Number:$ATOMIC_NUMBER - $NAME - $SYMBOL  $TYPE $ATOMIC_MASS  $MELTING_POINT $BOILING_POINT"
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 done
 
 fi
